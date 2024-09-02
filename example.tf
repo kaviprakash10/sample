c7615658414603150714842b0a931f84ac218c2e
kaviprakash <kaviprakash2383@gmail.com>
2024-09-02 14:35:13 +0530

    Create example.tf

---
+++ example.tf
@@ -0,0 +1,3 @@
+Git fetch 
+ This command that tells your local repo git to retrive the latest update in the remote repo. It's more like just checking to see if there are any changes are available.
+ 
