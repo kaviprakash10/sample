cf2d1920d0141ac827d32b1e464d828551130e1e
kaviprakash <kaviprakash2383@gmail.com>
2024-09-02 15:03:53 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,3 +1,1 @@
-Git fetch 
- This command that tells your local repo git to retrive the latest update in the remote repo. It's more like just checking to see if there are any changes are available.
- 
+hello
